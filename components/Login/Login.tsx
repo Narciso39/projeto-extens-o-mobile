0d0e3a43d529e990186d5d037ea3bbf51a9ea57b
Luis Felipe <felipenarciso15@gmail.com>
2025-04-14 02:31:14 -0300

    innitial commit

---
+++ components/Login/Login.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { Pressable, Text, TextInput, View } from "react-native";
+import { styles } from "./styles";
+
+const Login: React.FC = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.formTitle}> Texto formulario</Text>
+      <TextInput
+        style={styles.formInput}
+        placeholder="Email"
+        keyboardType="email-address"
+        autoCapitalize="none"
+        autoComplete="email"
+      />
+      <TextInput style={styles.formInput} 
+           placeholder='informe a senha'
+           autoCapitalize='none'
+           secureTextEntry
+      
+      />
+      <Pressable style={styles.formButton}>
+        <Text style={styles.textButton}>Entrar</Text>
+      </Pressable>
+      <View style={styles.subContainer}>
+        <Pressable style={styles.subButton}>
+          <Text style={styles.subTextButton}>Esqueci a senha</Text>
+        </Pressable>
+        <Pressable style={styles.subButton}>
+          <Text style={styles.subTextButton}>Cadastrar-se</Text>
+        </Pressable>
+      </View>
+    </View>
+  );
+};
+
+export default Login;
