484c083428387bc1eee80772e0ae69909e8cb7a4
Luis felipe <felipenarciso15@gmail.com>
2025-04-27 13:31:25 -0300

    página de registrar pronta, só falta estilizar

--- pages/Register/Register.tsx
+++ pages/Register/Register.tsx
@@ -1,8 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import { Pressable, Text, TextInput, View } from "react-native";
 import { styles } from "./styles";
+import { userRegister } from "@/services/user/register/register";
+import { useNavigation } from "expo-router";
+import { RootStackParamList } from "@/src/@types/routes.types";
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
+
+type NavigationProps = NativeStackNavigationProp<RootStackParamList, "Home">;
 
 const Register: React.FC = () => {
+  const [name, setName] = useState<any>("");
+  const [email, setEmail] = useState<any>("");
+  const [password, setPassword] = useState<any>("");
+  const navigation = useNavigation<NavigationProps>();
+
+  const handleRegister = async () => {
+    try {
+      await userRegister(name, email, password);
+      // console.log(response);
+        navigation.navigate("Login");
+     
+    } catch (error) {
+      console.error("Login falhou:", error);
+    }
+  };
+
   return (
     <View style={styles.container}>
       <Text style={styles.formTitle}></Text>
@@ -11,6 +33,8 @@ const Register: React.FC = () => {
         placeholder="Name"
         keyboardType="default"
         autoCapitalize="none"
+        onChangeText={(name) => setName(name)}
+        value={name}
       />
       <TextInput
         style={styles.formInput}
@@ -18,14 +42,21 @@ const Register: React.FC = () => {
         keyboardType="email-address"
         autoCapitalize="none"
         autoComplete="email"
+        onChangeText={(email) => setEmail(email)}
+        value={email}
       />
-      <TextInput style={styles.formInput} 
-           placeholder='informe a senha'
-           autoCapitalize='none'
-           secureTextEntry
+      <TextInput
+        style={styles.formInput}
+        placeholder="Senha"
+        autoCapitalize="none"
+        secureTextEntry
+        onChangeText={(password) => setPassword(password)}
+        value={password}
       />
-      <Pressable style={styles.formButton}>
-        <Text style={styles.textButton} onPress={}>Cadastrar</Text>
+      <Pressable style={styles.formButton} onPress={handleRegister}>
+        <Text style={styles.textButton} >
+          Cadastrar
+        </Text>
       </Pressable>
     </View>
   );
--- services/user/register/register.ts
+++ services/user/register/register.ts
@@ -1,5 +1,25 @@
 import api from "@/services/api";
 
-export const userRegister = () => {
+export const userRegister = async (name: string, email: string, password: string) => {
+    try {
+        const result = await api.post(
+          "/users",
+          {
+            name,
+            email,
+            password,
+          },
+          {
+            headers: {
+              "Content-Type": "application/json",
+            },
+          }
+        );
     
+        //   console.log(result.data);
+        return result.data;
+      } catch (error) {
+        console.error("Erro ao logar:", error);
+        throw error;
+      }
 }
